2ca35ce43c2ab1e10c7460754f7c6915d71c4432
Alex Dewar <a.dewar@imperial.ac.uk>
2025-03-19 15:06:56 +0000

    Check test table data conforms to schema

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,15 +1,37 @@
+"""Tests for the table JSON schema file."""
+
 import json
 from pathlib import Path
 
+import pytest
 from jsonschema import Draft4Validator
 
-_REPO_PATH = Path(__file__).parent.parent
+_TESTS_PATH = Path(__file__).parent
+_REPO_PATH = _TESTS_PATH.parent
 _SCHEMA_PATH = _REPO_PATH / "keyFiles" / "table_schema.json"
 
 
-def test_table_schema_valid():
-    """Test whether the table JSON schema is valid."""
+@pytest.fixture
+def table_schema():
+    """The parsed contents of the table schema JSON file."""
     with _SCHEMA_PATH.open() as f:
-        schema = json.load(f)
+        return json.load(f)
 
-    Draft4Validator.check_schema(schema)
+
+def test_table_schema_valid(table_schema):
+    """Test whether the table JSON schema is valid."""
+    Draft4Validator.check_schema(table_schema)
+
+
+def test_table_output_files_valid(table_schema):
+    """Test whether table output JSON files in data folder are valid."""
+    files = [
+        path for path in (_TESTS_PATH / "data").rglob("*_tables.json") if path.is_file()
+    ]
+    assert files
+
+    validator = Draft4Validator(table_schema)
+    for file in files:
+        with open(file) as f:
+            data = json.load(f)
+        assert validator.is_valid(data)
