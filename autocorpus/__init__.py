6e2779446f5e8510bb6999df12eb5a86639331a6
Alex Dewar <a.dewar@imperial.ac.uk>
2025-03-20 12:32:41 +0000

    Add default logger for AC
    It's not used anywhere yet.

--- autocorpus/__init__.py
+++ autocorpus/__init__.py
@@ -1,5 +1,28 @@
 """AutoCorpus package."""
 
+import logging
 from importlib.metadata import version
 
 __version__ = version(__name__)
+
+
+def create_logger():
+    """Create a logger for the program with default settings."""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # create console handler
+    ch = logging.StreamHandler()
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    ch.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(ch)
+
+    return logger
+
+
+logger = create_logger()
+"""The logger for the Auto-CORPus module and command-line program."""
