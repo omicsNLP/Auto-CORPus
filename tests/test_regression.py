bdff2854c4c986c80fd89062bac066ffcbace2c9
Thomas Rowlands <Thomas.s.Rowlands@gmail.com>
2025-03-26 23:16:34 +0000

    Added a test for the current version of PMC articles, using the pmc config.

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -7,14 +7,65 @@ from typing import Any
 from autocorpus.configs.default_config import DefaultConfig
 
 
-def test_autocorpus(data_path: Path):
-    """A regression test for the main Auto-CORPus class on the Auto-CORPus paper.
+def test_pmc_autocorpus(data_path: Path):
+    """A regression test for the main autoCORPus class, using the current PMC config on the AutoCORPus Paper.
+
+    The test data output is built by running Auto-CORPus from the root of the repo with the
+    following arguments
+    ```
+    auto-corpus -b "PMC" -t "tests/data/PMC/Current/" -f "tests/data/PMC/Current/PMC8885717.html"
+    ```
+    """
+    from autocorpus.Autocorpus import Autocorpus
+
+    pmc_example_path = data_path / "PMC" / "Current"
+    with open(
+        pmc_example_path / "PMC8885717_abbreviations.json",
+    ) as f:
+        expected_abbreviations = json.load(f)
+    with open(
+        pmc_example_path / "PMC8885717_bioc.json",
+        encoding="utf-8",
+    ) as f:
+        expected_bioc = json.load(f)
+    with open(
+        pmc_example_path / "PMC8885717_tables.json",
+        encoding="utf-8",
+    ) as f:
+        expected_tables = json.load(f)
+
+    auto_corpus = Autocorpus(
+        config=DefaultConfig.PMC.load_config(),
+        base_dir=str(pmc_example_path),
+        main_text=str(pmc_example_path / "PMC8885717.html"),
+    )
+
+    auto_corpus.process_files()
+
+    abbreviations = auto_corpus.abbreviations
+    bioc = auto_corpus.to_bioc()
+    tables = auto_corpus.tables
+
+    _make_reproducible(
+        abbreviations,
+        expected_abbreviations,
+        bioc,
+        expected_bioc,
+        tables,
+        expected_tables,
+    )
+    assert abbreviations == expected_abbreviations
+    assert bioc == expected_bioc
+    assert tables == expected_tables
+
+
+def test_legacy_pmc_autocorpus(data_path: Path):
+    """A regression test for the main autoCORPus class, using the legacy PMC config on the AutoCORPus Paper.
 
     The test data output is built by running Auto-CORPus from the root of the repo with the
     following arguments:
-
     ```
-    auto-corpus -b LEGACY_PMC -t tests/data/PMC/Pre-Oct-2024/ -f tests/data/PMC/Pre-Oct-2024/PMC8885717.html
+    auto-corpus -b "LEGACY_PMC" -t "tests/data/PMC/Pre-Oct-2024/" -f "tests/data/PMC/Pre-Oct-2024/PMC8885717.html"
     ```
     """
     from autocorpus.Autocorpus import Autocorpus
